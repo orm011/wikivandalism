64aa3cebd2888e69a92603867b462ebe8c277815
Geza Kovacs <gkovacs@mit.edu>
2012-04-25 23:25:59 -0400

    script to automate generation of csv data

---
+++ featuredata.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import wikipedia_request
+import sys
+
+featurename = sys.argv[1]
+if featurename[-3:] == '.py':
+  featurename = featurename[:-3]
+featurefn = getattr(wikipedia_request, featurename)
+numexamples = 10
+if len(sys.argv) > 2:
+  numexamples = int(sys.argv[2])
+wikipedia_request.feature_to_text(featurefn, numexamples)
+
